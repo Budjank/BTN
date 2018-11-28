9d531513065e8e760602b3abf8d873c5bafde9bc
Budjank <42899293+Budjank@users.noreply.github.com>
2018-11-28 14:40:20 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# BTN
-Bot Toto News
+# toto_news
+bot aplikasi ToTo News silakan dicoba jika ada kesalahan silakan lapor.. jadilah smart user
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBioDQutCBa7_blcu9en5rfYbM2YK5hpqzhZNjkLC4zIugfNGuqpylv9uLz62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ
+";
+$meid = "154a24140d9f8a40";
+$os="android";
+$sign="1f86479ad6fd267ebceaecb8312ff2db3bf84ee61850919cd0854b9a1d332a68";
